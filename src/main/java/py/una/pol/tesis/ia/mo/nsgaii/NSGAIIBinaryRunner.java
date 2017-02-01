82c7a8a786901e6c14aa3c9d8bde48161aa2a9ae
Arturo Molinas <arturomolinas@gmail.com>
2017-01-31 22:23:37 -0300

    cambios

--- src/main/java/py/una/pol/tesis/ia/mo/nsgaii/NSGAIIBinaryRunner.java
+++ src/main/java/py/una/pol/tesis/ia/mo/nsgaii/NSGAIIBinaryRunner.java
@@ -15,7 +15,12 @@ import org.uma.jmetal.util.AlgorithmRunner;
 import org.uma.jmetal.util.JMetalLogger;
 
 import java.util.List;
-import org.uma.jmetal.problem.multiobjective.zdt.ZDT5;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import py.una.pol.tesis.ia.mo.problemas.SEProblemMO;
 
 public class NSGAIIBinaryRunner extends AbstractAlgorithmRunner {
 
@@ -35,20 +40,20 @@ public class NSGAIIBinaryRunner extends AbstractAlgorithmRunner {
             referenceParetoFront = "";
         }
 
-        problem = new ZDT5();
+        problem = new SEProblemMO(obtenerMatrizDeLaImagen(""));//agregar la imagen
 
-        double crossoverProbability = 0.9;
+        double crossoverProbability = 0.75;
         crossover = new SinglePointCrossover(crossoverProbability);
 
         double mutationProbability = 0.025;//1.0 / problem.getNumberOfBits(0) ;
         mutation = new BitFlipMutation(mutationProbability);
 
-        selection = new BinaryTournamentSelection<BinarySolution>();
+        selection = new BinaryTournamentSelection<>();
 
-        algorithm = new NSGAIIBuilder<BinarySolution>(problem, crossover, mutation)
+        algorithm = new NSGAIIBuilder<>(problem, crossover, mutation)
                 .setSelectionOperator(selection)
-                .setMaxIterations(25000)
-                .setPopulationSize(100)
+                .setMaxIterations(800*40)
+                .setPopulationSize(40)
                 .build();
 
         AlgorithmRunner algorithmRunner = new AlgorithmRunner.Executor(algorithm)
@@ -64,5 +69,15 @@ public class NSGAIIBinaryRunner extends AbstractAlgorithmRunner {
         if (!referenceParetoFront.equals("")) {
             printQualityIndicators(population, referenceParetoFront);
         }
+    }
+
+    private static Mat obtenerMatrizDeLaImagen(String fileName) {
+        Mat imagen = Imgcodecs.imread(fileName, Imgcodecs.IMREAD_GRAYSCALE);
+        imagen.reshape(256, 256);
+
+        Mat rz = new Mat();
+        Imgproc.resize(imagen, rz, new Size(256, 256));
+        rz.convertTo(rz, CvType.CV_8UC1);
+        return rz;
     }
 }
